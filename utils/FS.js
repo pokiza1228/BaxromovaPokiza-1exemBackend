e63b50f7822c53217d40c14545884417f4645eaf
pokiza1228 <baxromovapokiza1@gmail.com>
2022-07-05 14:38:57 +0500

    API

--- utils/FS.js
+++ utils/FS.js
@@ -3,7 +3,6 @@ const path=require('path')
 const read=(dir)=>{
     return JSON.parse(fs.readFileSync(path.resolve(__dirname,`../${dir}`)))
 }
-
 const write=(dir,data)=>{
     fs.writeFileSync(path.resolve(__dirname,`../${dir}`),JSON.stringify(data,null,4))
 }
